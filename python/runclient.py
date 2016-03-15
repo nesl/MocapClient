f905f589970f579acc8f68504c7bcab3a2b06358
Chenguang Shen <pscggeforce@gmail.com>
2016-03-15 15:40:34 -0700

    Added timestamp for output datastream.

--- python/runclient.py
+++ python/runclient.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 # --- IMPORTS ---
+import time
 from Mocap import *
 
 mc = MocapClient('224.0.0.1', 1510, 1511)
+tstart = time.time()
+
 for frame in mc.listen():
-	print 'Received frame with ID: %d' % frame.getFrameNum()
-	print '-----------------------------------'
+	# print 'Received frame with ID: %d' % frame.getFrameNum()
+	# print '-----------------------------------'
+	t = time.time() - tstart
 	for rb in frame.getRigidBodies():
-		print rb
-	print ''
+		print t*1e3, rb.getPosition()[0], rb.getPosition()[1], rb.getPosition()[2], rb.getQuaternion()[0], rb.getQuaternion()[1], rb.getQuaternion()[2]
+	# print ''
